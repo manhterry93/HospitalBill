a9dd4b91b94cac57f6e59485df207afe9dba74a9
manhterry93 <manhterry93@gmail.com>
2018-05-28 02:14:58 +0700

    Add missing files

---
+++ Sources/app/src/main/java/bkhn/et/hospitalbill/data/model/InsuranceItem.java
@@ -0,0 +1,26 @@
+package bkhn.et.hospitalbill.data.model;
+
+/**
+ * Created by PL_itto on 5/28/2018.
+ */
+
+public class InsuranceItem {
+    String id;
+    double percent;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+}
